68f0f8caf33468829e1f004ea79a489bf45f758e
Jean Hamm <jeanmichehamm@gmail.com>
2023-04-20 14:24:08 -0300

    v2

--- main copy.js
+++ main copy.js
@@ -1,5 +1,12 @@
+//Busca o form de envio no HMTl
 const form = document.querySelector('#novoItem');
+
+//busta a lista no HTML
 const lista = document.getElementById('lista');
+
+//Array para salvar no local Storage
+//Ele foi salvo em string, usando o JSON.parse para transforar ele em array
+//usou o localStorega.getitem para pegar da memoria o itens em parenteses
 const itens = JSON.parse(localStorage.getItem('itens'))  || [];
 
 
@@ -9,25 +16,18 @@ itens.forEach(function(element){
 
 
 form.addEventListener('submit', function(event){
+    //Ele seta como default o evento
     event.preventDefault()
     
+    //ele identifica dentro do form a caracteristica elements que tem um nome para cada item
     const nome = event.target.elements['nome'];
     const quantidade = event.target.elements['quantidade'];
 
-    const existe = itens.find(elemento => elemento.nome === nome.value);
-
+    //Criada uma array para conter o nome e quantidade
     const itemAtual = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
-
-    if(existe){
-        itemAtual.id = existe.id
-        console.log(existe.id)
-    }else{
-        itemAtual.id = itens.lenght
-    }
-
     
     nome.focus();
 
@@ -37,9 +37,14 @@ form.addEventListener('submit', function(event){
 
     criaElemento(itemAtual) 
 
+    //Usado para adicionar o item atual dentro da array dos itens
     itens.push(itemAtual);
 
+    //Usado para salvar no navegador os dadaos alvos
+    //usado o JSON.stringify para trasnformar o array em texto, pois o navegador só armageza strings
     localStorage.setItem('itens', JSON.stringify(itens))
+
+        //Usado para apagar os dados no form
         nome.value='';
         quantidade.value='';
      
@@ -47,16 +52,22 @@ form.addEventListener('submit', function(event){
 
 function criaElemento(item){
     
+    //Criou um novo elemento li
     const novoItem = document.createElement('li');
+    // adicionou uma classe para ele 'item'
     novoItem.classList.add('item');
 
+    //criou um elemento strong
     const numeroItens = document.createElement('strong');
+    //colocou a quantidade dentro do strong
     numeroItens.innerHTML = item.quantidade;
-    numeroItens.dataset.id = item.id
 
+    //Colocou o strong dentro do li
     novoItem.appendChild(numeroItens);
+    //Colocou o nome dentro do li depois do strong
     novoItem.innerHTML += item.nome
 
+    // adiciona na lista o item que criamos
     lista.appendChild(novoItem);
 
 }
